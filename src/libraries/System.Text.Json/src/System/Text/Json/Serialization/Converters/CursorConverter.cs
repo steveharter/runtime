2b192ef494ecaa2409c36551656b72dfb6641e05
Steve Harter <steveharter@users.noreply.github.com>
2021-11-03 17:50:02 -0500

    Sync1

--- src/libraries/System.Text.Json/src/System/Text/Json/Serialization/Converters/CursorConverter.cs
+++ src/libraries/System.Text.Json/src/System/Text/Json/Serialization/Converters/CursorConverter.cs
@@ -5,19 +5,20 @@ using System.Diagnostics;
 
 namespace System.Text.Json.Serialization.Converters
 {
-    internal sealed partial class CursorConverter : JsonResumableConverter<bool> // bool is a dummy
+    internal sealed class CursorConverter : JsonResumableConverter<bool> // bool is a dummy
     {
         private readonly JsonCursor _cursor;
 
-        private bool _continuation_propertyName;
+        private bool _continuation_readObject;
+        private bool _continuation_readArray;
         private bool _continuation_skipping;
+        private readonly JsonPath _destinationPath;
+        private int _arrayIndex;
 
-        private int _levelCount;
-        private JsonLevel[]? _levels;
-
-        public CursorConverter(JsonCursor jsonCursor)
+        public CursorConverter(JsonCursor jsonCursor, JsonPath jsonPath)
         {
             _cursor = jsonCursor;
+            _destinationPath = jsonPath;
         }
 
         internal sealed override ConverterStrategy ConverterStrategy => ConverterStrategy.JsonCursor;
@@ -29,6 +30,7 @@ namespace System.Text.Json.Serialization.Converters
             ref ReadStack state,
             out bool value)
         {
+        TryRead:
             if (_continuation_skipping)
             {
                 if (!reader.TrySkip())
@@ -39,7 +41,9 @@ namespace System.Text.Json.Serialization.Converters
 
                 _continuation_skipping = false;
             }
-            else if (_continuation_propertyName)
+
+        TryReadProperty:
+            if (_continuation_readObject)
             {
                 if (!reader.Read())
                 {
@@ -47,175 +51,123 @@ namespace System.Text.Json.Serialization.Converters
                     return false;
                 }
 
-                ReadPropertyName(ref reader);
-                if (IsAtDestination())
+                string? propertyName = reader.GetString();
+                Debug.Assert(propertyName != null);
+
+                if (_destinationPath._elements[reader.CurrentDepth].Name == propertyName)
                 {
-                    value = true;
-                    return true;
+                    // The current property matches; see if we are done.
+                    if (_destinationPath._elements.Length == reader.CurrentDepth)
+                    {
+                        value = true;
+                        return true;
+                    }
+
+                    // We need to read the value and recurse.
+                    _continuation_readObject = false;
+                    goto ReadNext;
                 }
+                else
+                {
+                    // Skip the property value.
+                    if (!reader.TrySkip())
+                    {
+                        _continuation_skipping = true;
+                        goto TryRead;
+                    }
+                }
+
+                while (reader.Read())
+                {
+                    JsonTokenType jsonTokenType = reader.TokenType;
+                    switch (jsonTokenType)
+                    {
+                        case JsonTokenType.PropertyName:
+                            goto TryReadProperty;
+                        case JsonTokenType.EndObject:
+                            // Property was not found.
+                            value = false;
+                            return true;
+                        default:
+                            Debug.Assert(false, $"Token {jsonTokenType} not expected");
+                            value = false;
+                            return true;
+                    }
+                }
+
+                value = false;
+                return false;
             }
 
+        TryReadArray:
+            if (_continuation_readArray)
+            {
+                if (_destinationPath._elements[reader.CurrentDepth].Name != null)
+                {
+                    // Expected an array
+                    value = false;
+                    return true;
+                }
+
+                // Position reader on element
+                while (reader.Read())
+                {
+                    if (reader.TokenType == JsonTokenType.EndArray)
+                    {
+                        // Element was not found.
+                        value = false;
+                        return true;
+                    }
+
+                    if (_destinationPath._elements[reader.CurrentDepth].Index == _arrayIndex)
+                    {
+                        // The current index matches; see if we are done.
+                        if (_destinationPath._elements.Length == reader.CurrentDepth)
+                        {
+                            value = true;
+                            return true;
+                        }
+
+                        // Read the value and recurse.
+                        _continuation_readArray = false;
+                        goto ReadNext;
+                    }
+
+                    // Skip if object\array
+                    if (!reader.TrySkip())
+                    {
+                        _continuation_skipping = true;
+                        goto TryRead;
+                    }
+
+                    _arrayIndex++;
+                }
+
+                value = false;
+                return false;
+            }
+
+
+        ReadNext:
             while (reader.Read())
             {
                 JsonTokenType jsonTokenType = reader.TokenType;
                 switch (jsonTokenType)
                 {
                     case JsonTokenType.PropertyName:
-                        Debug.Assert(GetLevels()[_levelCount].TokenType == JsonTokenType.StartObject);
+                        // First property for an object.
+                        _continuation_readObject = true;
+                        goto TryReadProperty;
 
-                        if (!reader.Read())
-                        {
-                            _continuation_propertyName = true;
-                            value = false;
-                            return false;
-                        }
-
-                        ReadPropertyName(ref reader);
-                        if (IsAtDestination())
-                        {
-                            value = true;
-                            return true;
-                        }
-
-                        if (ShouldSkip())
-                        {
-                            if (reader.TrySkip())
-                            {
-                                _continuation_skipping = true;
-                                value = false;
-                                return false;
-                            }
-                        }
-
-                        break;
-
-                    case JsonTokenType.StartObject:
                     case JsonTokenType.StartArray:
-                        {
-                            JsonLevel[] levels = GetLevels();
-                            levels[_levelCount].TokenType = jsonTokenType;
-                            if (_levelCount == 0)
-                            {
-                                levels[_levelCount].JsonPath = "$";
-                            }
-                            else if (jsonTokenType == JsonTokenType.StartObject)
-                            {
-                                // todo: avoid this if dest doesn't contain properties
-                                levels[_levelCount].JsonPath = levels[_levelCount - 1].JsonPath + "." + levels[_levelCount].CurrentPropertyName;
-                            }
-                            else
-                            {
-                                // todo: avoid this if dest doesn't contain []
-                                Debug.Assert(jsonTokenType == JsonTokenType.StartArray);
-                                levels[_levelCount].JsonPath = levels[_levelCount - 1].JsonPath + "[" + levels[_levelCount].CurrentArrayLength + "]";
-                            }
-
-                            if (IsAtDestination())
-                            {
-                                value = true;
-                                return true;
-                            }
-
-                            _levelCount++;
-                            break;
-                        }
-
-                    case JsonTokenType.EndObject:
-                    case JsonTokenType.EndArray:
-                        _levelCount--;
-
-                        // todo: path for SkipToEnd
-
-                        break;
-
-                    default:
-                        {
-                            Debug.Assert(
-                                jsonTokenType == JsonTokenType.False ||
-                                jsonTokenType == JsonTokenType.True ||
-                                jsonTokenType == JsonTokenType.Null ||
-                                jsonTokenType == JsonTokenType.Number ||
-                                jsonTokenType == JsonTokenType.String);
-
-                            JsonLevel[] levels = GetLevels();
-                            JsonTokenType currentParentTokenType = levels[_levelCount - 1].TokenType;
-                            Debug.Assert(
-                                currentParentTokenType == JsonTokenType.StartArray ||
-                                currentParentTokenType == JsonTokenType.StartObject);
-
-                            if (currentParentTokenType == JsonTokenType.StartArray)
-                            {
-                                _levels![_levelCount - 1].CurrentArrayLength = _levels[_levelCount - 1].CurrentArrayLength + 1;
-                            }
-
-                            break;
-                        }
+                        _continuation_readArray = true;
+                        _arrayIndex = 0;
+                        goto TryReadArray;
                 }
             }
 
             value = false;
             return false;
-
-            void ReadPropertyName(ref Utf8JsonReader reader)
-            {
-                Debug.Assert(_continuation_propertyName == false);
-
-                string? propertyName = reader.GetString();
-                if (propertyName == null)
-                {
-                    throw new JsonException("todo; null property name not supported");
-                }
-
-                GetLevels()[_levelCount -1].CurrentPropertyName = propertyName;
-            }
-
-            bool IsAtDestination()
-            {
-                return GetLevels()[_levelCount - 1].JsonPath!.Equals(_destJsonPath, StringComparison.OrdinalIgnoreCase);
-            }
-
-            bool ShouldSkip()
-            {
-                JsonLevel level = GetLevels()[_levelCount - 1];
-                string jsonPath = level.JsonPath!;
-                int i = _destJsonPath.IndexOf(jsonPath);
-                if (i < 0)
-                {
-                    return true;
-                }
-
-                if (level.TokenType == JsonTokenType.StartObject)
-                {
-                    Debug.Assert(level.CurrentPropertyName != null);
-                    // Skip if current property does not match.
-                    return _destJsonPath.IndexOf(level.CurrentPropertyName, i) == i;
-                }
-                else
-                {
-                    Debug.Assert(level.TokenType == JsonTokenType.StartArray);
-                    // Skip if current index does not match.
-                    return level.CurrentArrayLength != _levelCount;
-                }
-            }
-
-            JsonLevel[] GetLevels()
-            {
-                if (_levels == null)
-                {
-                    _levels = new JsonLevel[JsonLevel.DefaultLevelSize];
-                }
-                else if (_levels.Length == _levelCount)
-                {
-                    Array.Resize(ref _levels, _levels.Length * 2);
-                }
-                else
-                {
-                    // Initalize in case re-used in a different tree branch.
-                    _levels[_levelCount - 1].Clear();
-                }
-
-                return _levels;
-            }
         }
 
         internal override bool OnTryWrite(
